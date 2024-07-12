20e2511d488f65fa3649c0d515525d1b5ee02269
Gregor Kirbiš <gregor.kirbis@gmail.com>
2024-07-12 12:49:29 +0200

    first from Gregor Kirbiš

---
+++ react-dates-test/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import DatePicker from './DatePicker';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Dates Test</h1>
+        <DatePicker />
+      </header>
+    </div>
+  );
+}
+
+export default App;
