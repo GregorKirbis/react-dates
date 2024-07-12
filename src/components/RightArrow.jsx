c0d5af3df36c801e55ecab75694b292716eaaa11
Gregor Kirbiš <gregor.kirbis@gmail.com>
2024-07-12 21:27:29 +0200

    !

--- src/components/RightArrow.jsx
+++ src/components/RightArrow.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const RightArrow = ({ size = 24, color = 'black', ...props }) => (
+const RightArrow = ({ ...props }) => (
   <svg
     focusable="false" viewBox="0 0 1000 1000"
     xmlns="http://www.w3.org/2000/svg"
