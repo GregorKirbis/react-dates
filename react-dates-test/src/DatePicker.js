3321d430469b19e015d533dd1ece97bd346d1ddd
Gregor Kirbiš <gregor.kirbis@gmail.com>
2024-07-13 14:49:44 +0200

    fix some compatibility problems

---
+++ react-dates-test/src/DatePicker.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { DateRangePicker } from 'react-dates';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+
+const DatePicker = () => {
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [focusedInput, setFocusedInput] = useState(null);
+
+  return (
+    <div>
+      <DateRangePicker
+        startDate={startDate}
+        startDateId="your_unique_start_date_id"
+        endDate={endDate}
+        endDateId="your_unique_end_date_id"
+        onDatesChange={({ startDate, endDate }) => {
+          setStartDate(startDate);
+          setEndDate(endDate);
+        }}
+        focusedInput={focusedInput}
+        hideKeyboardShortcutsPanel ={true}
+        onFocusChange={focusedInput => setFocusedInput(focusedInput)}
+        numberOfMonths={1}
+        isOutsideRange={() => false}
+        displayFormat="DD/MM/YYYY"
+      />
+    </div>
+  );
+};
+
+export default DatePicker;
